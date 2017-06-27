ddb672f5c5cbcd2a00536fe7e884e6fc6115f361
usman-tahir <tahir.usman.ali94@outlook.com>
2017-06-27 16:24:11 -0400

    added a simple greeter application using webpack

---
+++ greeting-example/assets/js/greeter.js
@@ -0,0 +1,9 @@
+'use strict';
+
+function greet() {
+	console.log('Have a nice day!');
+};
+
+// export default greet;
+
+module.exports = greet;
---
+++ greeting-example/assets/js/index.js
@@ -0,0 +1,9 @@
+(function () {
+	'use strict';
+
+	// import greet from './greeter.js';
+	var greet = require('./greeter.js');
+
+	console.log('I\'m the entry point');
+	greet();
+}());
